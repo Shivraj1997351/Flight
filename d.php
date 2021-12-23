d35810439f7ffd184d090c5d54a81e9ba3f7110d
Shivraj1997351 <rudravishal123@gmail.com>
2021-12-23 22:35:53 +0530

    connected to infinityFree

--- d.php
+++ d.php
@@ -1,8 +1,8 @@
 <?php
-$con = mysqli_connect("localhost:1433","root","","shiv");
+$con = mysqli_connect("sql112.epizy.com","epiz_30657547","yX9wq9IjMrE5O7p","epiz_30657547_shiv");
 if (!$con)
   {
   die("Website is under maintainance Please try again later!");
   }
-//localhost:1433
+//localhost:1433,root,"",shiv
 ?>
